0d9829044f0b3bbe39bff4e192128edf80fb57b6
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-10-08 12:33:08 +0900

    feat: layered architecture

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,81 +1,32 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use anyhow::Result;
-use axum::{extract::State, http::StatusCode, routing::get, Router};
-use sqlx::{postgres::PgConnectOptions, PgPool};
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let database_cfg = DatabaseConfig {
-        host: "localhost".into(),
-        port: 5432,
-        username: "app".into(),
-        password: "passwd".into(),
-        database: "app".into(),
-    };
+    bootstrap().await
+}
 
-    let conn_pool = connect_database_with(database_cfg);
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+    let pool = connect_database_with(&app_config.database);
+
+    let registry = AppRegistry::new(pool);
 
     let app = Router::new()
-        .route("/health", get(health_check))
-        .route("/health/db", get(health_check_db))
-        .with_state(conn_pool);
+        .merge(build_health_check_routers())
+        .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(&addr).await?;
 
-    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on {}", addr);
 
-    println!("Linstening on {}", addr);
-
-    Ok(axum::serve(listener, app).await?)
-}
-
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-pub async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
-    let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
-
-    match connection_result {
-        Ok(_) => StatusCode::OK,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    let status_code = health_check().await;
-
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-#[sqlx::test]
-async fn health_check_db_works(pool: sqlx::PgPool) {
-    let status_code = health_check_db(State(pool)).await;
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-struct DatabaseConfig {
-    pub host: String,
-    pub port: u16,
-    pub username: String,
-    pub password: String,
-    pub database: String,
-}
-
-impl From<DatabaseConfig> for PgConnectOptions {
-    fn from(cfg: DatabaseConfig) -> Self {
-        Self::new()
-            .host(&cfg.host)
-            .port(cfg.port)
-            .username(&cfg.username)
-            .password(&cfg.password)
-            .database(&cfg.database)
-    }
-}
-
-fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
-    PgPool::connect_lazy_with(cfg.into())
+    axum::serve(listener, app).await.map_err(Error::from)
 }
